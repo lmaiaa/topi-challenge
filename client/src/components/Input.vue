1d2bc066f2594635dbdb35450bf177b0d9e16f10
Luccas Maia <luccas.maia@finxi.com.br>
2021-03-07 10:04:27 -0300

    client: add input default

--- client/src/components/Input.vue
+++ client/src/components/Input.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="slds-box slds-box_small slds-align_absolute-center slds-m-top_large" style="width:300px;">
     <div class="slds-form-element">
-      <label class="slds-form-element__label" for="text-input-id-1">Search your meal</label>
+      <label class="slds-form-element__label" for="text-input-id-1">Search by name</label>
       <div class="slds-form-element__control slds-input-has-icon slds-input-has-icon_left">
         <svg class="slds-icon slds-input__icon slds-input__icon_left slds-icon-text-default" aria-hidden="true">
           <use xlink:href="@/assets/icons/utility-sprite/svg/symbols.svg#search"></use>
         </svg>
-        <input type="text" id="text-input-id-1" placeholder="Ex.: Rice" class="slds-input" />
+        <input
+          type="text"
+          id="text-input-id-1"
+          placeholder="Ex.: Rice"
+          class="slds-input"
+          @input="$emit('input', $event.target.value)"
+          :value="value"
+        />
       </div>
     </div>
   </div>
@@ -16,7 +23,10 @@
 import { defineComponent } from '@vue/composition-api';
 
 export default defineComponent({
-  name: 'SearchEngine'
+  name: 'SearchEngine',
+  props: {
+    value: { type: [String, Number] }
+  }
 });
 </script>
 
