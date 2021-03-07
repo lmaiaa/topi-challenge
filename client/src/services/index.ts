126e4c71e0e4b0cd112a3b551dfa841dc03e866f
Luccas Maia <luccas.maia@finxi.com.br>
2021-03-07 10:07:25 -0300

    client: add list by filter category and area

--- client/src/services/index.ts
+++ client/src/services/index.ts
@@ -1,6 +1,12 @@
 import { api } from '@/utils/api.instance';
 import { GetMealsResponse } from '@serverTypes/meals';
 
-export const listMeals = (name = '') => {
-  return api.get<never, GetMealsResponse>(`/meals/list?name=${name}`);
+export const listMealsByName = (name = '') => {
+  return api.get<never, GetMealsResponse>(`/meals?name=${name}`);
+};
+export const listMealsByCategory = (category = '') => {
+  return api.get<never, GetMealsResponse>(`/meals?category=${category}`);
+};
+export const listMealsByArea = (area = '') => {
+  return api.get<never, GetMealsResponse>(`/meals?area=${area}`);
 };
